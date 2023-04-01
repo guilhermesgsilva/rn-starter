3d3211ce7e237df138f5add8a5a00b945c08d28d
Guilherme Silva <silva.gsg@gmail.com>
2023-04-01 17:46:39 +0100

    feat: Add ListScreen content

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, FlatList } from "react-native";
 
 const ListScreen = () => {
+  const friends = [
+    { name: "Alice" },
+    { name: "Bob" },
+    { name: "Charlie" },
+    { name: "Dave" },
+    { name: "Emily" },
+    { name: "Frank" },
+    { name: "Grace" },
+    { name: "Henry" },
+    { name: "Isabella" },
+    { name: "Jack" },
+  ];
 
   return (
     <View>
       <Text style={styles.title}>List Screen</Text>
+      <FlatList
+        keyExtractor={(item) => item.name}
+        data={friends}
+        renderItem={({ item, index }) => {
+          // element === {item: {name: "Alice"}, index: 0}
+          return <Text>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
@@ -14,7 +34,6 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 30,
   },
-  
 });
 
 export default ListScreen;
