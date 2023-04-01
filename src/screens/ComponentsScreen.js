39664f52426f1e24e374c2fe60bd066502a09078
Guilherme Silva <silva.gsg@gmail.com>
2023-04-01 16:57:01 +0100

    feat: Update ComponentsScreen content

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the Components Screen.</Text>;
+  const name = "Guilherme";
+
+  return (
+    <View>
+      <Text style={styles.title}>Getting started with React Native!</Text>
+      <Text style={styles.subtitle}>My name is {name}.</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 30,
+  title: {
+    fontSize: 45,
+  },
+  subtitle: {
+    fontSize: 20,
   },
 });
 
