a69de1b8c212b6a9c51e83bd8f34518b0665d382
Guilherme Silva <silva.gsg@gmail.com>
2023-04-10 16:53:55 +0100

    feat: Add CounterScreen content

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,14 @@
-import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <View>
       <Text style={styles.title}>Counter Screen</Text>
+      <Text style={styles.message}>Current Count: {counter}</Text>
+      <Button title="Increase" onPress={() => setCounter(counter + 1)} />
+      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
     </View>
   );
 };
@@ -12,6 +16,9 @@ const CounterScreen = () => {
 const styles = StyleSheet.create({
   title: {
     fontSize: 45,
+  },
+  message: {
+    fontSize: 30,
   },
 });
 
