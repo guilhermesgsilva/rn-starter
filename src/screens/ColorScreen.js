ffa9ee7cf630202f640caa5ade739321bf6034c5
Guilherme Silva <silva.gsg@gmail.com>
2023-04-11 12:10:35 +0100

    feat: Add ColorScreen content

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,10 +1,35 @@
-import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Text, StyleSheet, View, Button, FlatList } from "react-native";
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+};
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+
   return (
     <View>
       <Text style={styles.title}>Color Screen</Text>
+      <Button
+        title="Add New Color"
+        onPress={() => {
+          setColors([randomRgb(), ...colors]);
+        }}
+      />
+      <FlatList
+        keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View style={{ width: 100, height: 100, backgroundColor: item }} />
+          );
+        }}
+      />
     </View>
   );
 };
