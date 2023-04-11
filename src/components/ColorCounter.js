5fdf0fa6ed0306b124442a8e0e93a6555055d8ee
Guilherme Silva <silva.gsg@gmail.com>
2023-04-11 14:38:04 +0100

    feat: Add SquareScreen content

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Text, StyleSheet, View, Button } from "react-native";
+
+const COLOR_INCREMENT = 15;
+
+const ColorCounter = ({ title, setColor, color }) => {
+  const handleIncrease = () => {
+    if (color + COLOR_INCREMENT > 255) {
+      setColor(255);
+      return;
+    }
+    setColor(color + COLOR_INCREMENT);
+  };
+
+  const handleDecrease = () => {
+    if (color - COLOR_INCREMENT < 0) {
+      setColor(0);
+      return;
+    }
+    setColor(color - COLOR_INCREMENT);
+  };
+
+  return (
+    <View>
+      <Text style={styles.title}>{title}</Text>
+      <Button title={`Increase ${title}`} onPress={handleIncrease} />
+      <Button title={`Decrease ${title}`} onPress={handleDecrease} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+  },
+});
+
+export default ColorCounter;
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View } from "react-native";
+import ColorCounter from "../components/ColorCounter";
 
 const SquareScreen = () => {
+    const [red, setRed] = useState(0);
+    const [green, setGreen] = useState(0);
+    const [blue, setBlue] = useState(0);
+
+    console.log(`rgb(${red}, ${green}, ${blue})`)
+
   return (
     <View>
       <Text style={styles.title}>Square Screen</Text>
+      <View style={{ width: 100, height: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})` }}/>
+      <ColorCounter title="Red" color={red} setColor={setRed}/>
+      <ColorCounter title="Green" color={green} setColor={setGreen}/>
+      <ColorCounter title="Blue" color={blue} setColor={setBlue}/>
     </View>
   );
 };
